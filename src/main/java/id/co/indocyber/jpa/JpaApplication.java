a2ff894506ee7dd79bd05077e7863221673d16a0
aqira <aqira.kelana@gmail.com>
2024-04-05 09:33:37 +0700

    added queries example

--- src/main/java/id/co/indocyber/jpa/JpaApplication.java
+++ src/main/java/id/co/indocyber/jpa/JpaApplication.java
@@ -4,6 +4,7 @@ import id.co.indocyber.jpa.repositories.EmployeeRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class JpaApplication {
@@ -15,6 +16,50 @@ public class JpaApplication {
 
         var data = employeeRepository.findAll();
         data.forEach(employee -> System.out.println(employee.getFullName()));
+
+        testJpaQueries(employeeRepository);
+    }
+
+    private static void testJpaQueries(EmployeeRepository employeeRepository) {
+        nativeQueryExample(employeeRepository);
+        jpqlExample(employeeRepository);
+        queryCreationExample(employeeRepository);
+    }
+
+    private static void nativeQueryExample(EmployeeRepository employeeRepository) {
+        System.out.println("=".repeat(100) + "NATIVE QUERY" + "=".repeat(100));
+
+        System.out.println("-".repeat(100) + "Select Where First Name =" + "-".repeat(100));
+        employeeRepository.nativeQueryExample("Andi")
+                .forEach(employee -> System.out.println(employee.getFullName()));
+    }
+
+    private static void jpqlExample(EmployeeRepository employeeRepository) {
+        System.out.println("=".repeat(100) + "JAVA PERSISTENCE QUERY LANGUAGE" + "=".repeat(100));
+
+        System.out.println("-".repeat(100) + "Select Where First Name =" + "-".repeat(100));
+        employeeRepository.jpqlQueryExample("Jean")
+                .forEach(employee -> System.out.println(employee.getFullName()));
+
+        System.out.println("-".repeat(100) + "Select Where First Name LIKE" + "-".repeat(100));
+        employeeRepository.jpqlQueryLikeExample("Candra")
+                .forEach(employee -> System.out.println(employee.getFullName()));
+    }
+
+    private static void queryCreationExample(EmployeeRepository employeeRepository) {
+        System.out.println("=".repeat(100) + "QUERY CREATION" + "=".repeat(100));
+
+        System.out.println("-".repeat(100) + "findByFirstName" + "-".repeat(100));
+        employeeRepository.findByFirstName("Jean")
+                .forEach(employee -> System.out.println(employee.getFullName()));
+
+        System.out.println("-".repeat(100) + "findByIsActive" + "-".repeat(100));
+        employeeRepository.findByIsActive(false)
+                .forEach(employee -> System.out.println(employee.getFullName()));
+
+        System.out.println("-".repeat(100) + "findByIsActiveTrue" + "-".repeat(100));
+        employeeRepository.findByIsActiveTrue()
+                .forEach(employee -> System.out.println(employee.getFullName()));
     }
 
 }
--- src/main/java/id/co/indocyber/jpa/repositories/EmployeeRepository.java
+++ src/main/java/id/co/indocyber/jpa/repositories/EmployeeRepository.java
@@ -2,6 +2,37 @@ package id.co.indocyber.jpa.repositories;
 
 import id.co.indocyber.jpa.models.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    //https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
+    @Query(value = """
+            SELECT *
+            FROM Employees AS e
+            WHERE e.FirstName = :firstName
+            """,
+            nativeQuery = true)
+    List<Employee> nativeQueryExample(@Param("firstName") String firstName);
+
+    @Query(value = """
+            SELECT e
+            FROM Employee e
+            WHERE e.firstName = :firstName
+            """)
+    List<Employee> jpqlQueryExample(@Param("firstName") String firstName);
+
+    @Query(value = """
+            SELECT e
+            FROM Employee e
+            WHERE e.firstName LIKE %:firstName%
+            """)
+    List<Employee> jpqlQueryLikeExample(@Param("firstName") String firstName);
+
+    List<Employee> findByFirstName(String firstName);
+    List<Employee> findByIsActive(boolean isActive);
+    List<Employee> findByIsActiveTrue();
+
 }
